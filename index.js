e8dcc6cc89f23e6c1dfadc17b330f22b1ba4dbc1
Eitan Seri-Levi <eserilev@ucsc.edu>
2022-06-01 18:56:32 +0300

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use strict'
 
 import * as IPFS from 'ipfs-core'
-import *  as all from 'it-all'
-import {Blob} from 'node:buffer';
+import { base32 } from "multiformats/bases/base32"
+import { CID } from 'multiformats/cid'
+import { base64} from "multiformats/bases/base64"
+import { sha256 } from 'multiformats/hashes/sha2'
+import * as json from 'multiformats/codecs/json'
+import * as dagPB from '@ipld/dag-pb'
+
 
 async function readFile () {
     const node = await IPFS.create()
@@ -10,7 +15,7 @@ async function readFile () {
   
     console.log('Version:', version.version)
 
-    const stream = node.cat('QmPChd2hVbrJ6bfo3WBcTW4iZnpHm8TEzWkLHmLpXhF68A')
+    const stream = node.cat('QmQsUMUB7FXzFVivPUb4HgGBihsHpfUN9PEeMvM8BsmTVV')
     let data = '';
 
     for await (const chunk of stream) {
@@ -20,26 +25,40 @@ async function readFile () {
     console.log(data)
 }
 
-async function writeFile () {
-  const node = await IPFS.create()
-  const version = await node.version()
+async function pin(path, content) {
+    const node = await IPFS.create();
+    const version = await node.version();
 
-  console.log('Version:', version.version)
+    console.log("Version:", version.version);
 
-  const fileText = "Hello World"
+    const bytes = dagPB.encode({
+        Data: new TextEncoder().encode('tokensfarm is honest work'),
+        Links: []
+    })
 
-  const blob = new Blob([fileText], {type: 'text/plain; charset=utf-8'});
+    const fileAdded = await node.add({
+        path: 'path',
+        content: bytes,
+    });
 
-  const file = await node.content({
-    path: 'hello.txt',
-    content: blob
-  })
-
-  console.log('Added file:', file.path, file.cid.toString())
-
-  const data = await all(node.cat(file.cid))
-
-  console.log('Added file contents:', data)
+    console.log("Added file:", fileAdded.path, fileAdded.cid);
 }
-  
-writeFile()
+
+async function checksumHash() {
+    var cid = CID.parse("QmXfb2VTCD7KNEgARCamTpiPQJT43aqFm8GSgvGkhcs5hu")
+    console.log(cid)
+
+    const bytes = dagPB.encode({
+        Data: new TextEncoder().encode('tokensfarm is honest work'),
+        Links: []
+    })
+
+
+
+    const hash = await sha256.digest(bytes)
+    var cid = CID.create(0, dagPB.code, hash)
+    console.log(cid)
+}
+// readFile()
+// pin('test.txt', 'tokensfarm is honest work')
+checksumHash()
