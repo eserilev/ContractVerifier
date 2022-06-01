b305764981ad4e3aa957f42803dbeb67c5e1189c
Yoel <yoelzerbib7@gmail.com>
2022-06-01 15:23:23 +0300

    added pin function

---
+++ pinFile.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import * as IPFS from 'ipfs-core'
+
+async function pin(path, content) {
+    const node = await IPFS.create();
+    const version = await node.version();
+
+    console.log("Version:", version.version);
+
+    const fileAdded = await node.add({
+        path: path,
+        content: JSON.stringify(content),
+    });
+
+    console.log("Added file:", fileAdded.path, fileAdded.cid);
+}
+
+module.exports = { pin }
